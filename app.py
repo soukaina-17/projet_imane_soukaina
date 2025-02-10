a351c74e70176becb9a2916be527772234295034
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-02-10 14:58:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,71 @@
-from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-from werkzeug.utils import secure_filename
-import sqlite3
+from flask import Flask, request, jsonify, redirect, render_template
+from sqlalchemy import create_engine
+from datetime import datetime
 
-app = Flask(__name__)                                                                                                                  
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions   #com2
+db_path = 'sqlite:///bdd_recette.db'
 
-# Fonction pour créer une clé "authentifie" dans la session utilisateur
-def est_authentifie():
-    return session.get('authentifie')
+engine = create_engine(db_path)
 
-@app.route('/')
-def a():
-    return render_template('a.html')
+# Configuration de la base de données
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipe.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/lecture')
-def lecture():
-    if not est_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
-        return redirect(url_for('authentification'))
-        
+# Modèle de recette
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
-    # Connexion à la base de données
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
+    def __repr__(self):
+        return f'<Recipe {self.name}>'
 
-    # Exécution de la requête SQL pour insérer un nouveau client
-    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, "ICI"))
-    conn.commit()
-    conn.close()
-    return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
-                                                                                                                                       
-if __name__ == "__main__":
-  app.run(debug=True)
+# Route pour afficher et ajouter des recettes
+@app.route('/recipe', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        new_recipe = Recipe(name=name, description=description)
+        try:
+            db.session.add(new_recipe)
+            db.session.commit()
+            return redirect('/recipe')  # Rediriger vers la page de recette
+        except Exception:
+            return "Une erreur s'est produite"
+    else:
+        recipes = Recipe.query.order_by(Recipe.created_at)
+        return render_template('index.html', recipes=recipes)
 
+# Route pour supprimer une recette
+@app.route('/recipe/delete/<int:id>')
+def delete(id):
+    recipe_to_delete = Recipe.query.get_or_404(id)
+    try:
+        db.session.delete(recipe_to_delete)
+        db.session.commit()
+        return redirect('/recipe')  # Rediriger vers la page de recette
+    except Exception:
+        return "Une erreur s'est produite"
+
+# Route pour mettre à jour une recette
+@app.route('/recipe/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    recipe = Recipe.query.get_or_404(id)
+    if request.method == 'POST':
+        recipe.name = request.form['name']
+        recipe.description = request.form['description']
+
+        try:
+            db.session.commit()
+            return redirect('/recipe')  # Rediriger vers la page de recette
+        except Exception:
+            return "Une erreur s'est produite"
+    else:
+        return render_template('update.html', recipe=recipe)
+
+
+# Lancer l'application
+if __name__ == '__main__':
+    app.run(debug=True)
